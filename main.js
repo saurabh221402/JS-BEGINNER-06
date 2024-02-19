93421dee9386fa998c172dbad4a26ae7c630e4f2
saurabh gupta <sg072106@gmail.com>
2024-02-20 01:01:22 +0530

    complete

--- main.js
+++ main.js
@@ -6,18 +6,21 @@ function showPage()
 
 }
 
-
+let height= innerHeight;
 document.addEventListener("dragstart", function(event) {
     // The dataTransfer.setData() method sets the data type and the value of the dragged data
-    event.dataTransfer.setData("Text", event.target.id);
+   // event.dataTransfer.setData("Text", event.target.id);
     
    console.log(event);
   });
 
 
-//   document.addEventListener("drag", function(event) {
-//     document.getElementById("demo").style.color = "red";
-//   });
+  document.addEventListener("drag", function(event) {
+    let x=event.clientY;
+    let page=document.querySelector(".page");
+    page.style.top=x+"px";
+    page.style.height=height-x+10+"px";
+  });
 
 //   document.addEventListener("dragend", function(event) {
 //     document.getElementById("demo").innerHTML = "Finished dragging the p element.";
